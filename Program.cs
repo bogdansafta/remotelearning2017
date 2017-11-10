9b193b799a53ab90b50c5090b96038eb3013211c
BarthaRobert <33524939+BarthaRobert@users.noreply.github.com>
2017-11-10 16:55:43 +0200

    Add files via upload
    OOPHelloWorld - Tema

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace OOPHelloWorld
+{
+    public abstract class Animal
+    {
+        public String Name;
+        public abstract String MakeSound();
+        public Animal(String Name)
+        {
+            this.Name=Name;
+        }
+    }
+     public class Dog : Animal
+    {
+        public Dog(String Name) : base(Name) {} 
+       
+        public override String MakeSound()
+        {
+            String DogSound="HAM";
+            return DogSound;
+        }
+    }
+    public class Cat:Animal
+    {
+        public Cat(String Name) : base(Name){}
+        public override String MakeSound()
+        {
+            String CatSound="MIAU";
+            return CatSound;
+        }
+    }
+    public class Frog:Animal
+    {
+        public Frog(String Name) : base(Name){}
+        public override String MakeSound()
+        {
+            String FrogSound="OAC";
+            return FrogSound;
+        }
+    }
+
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+           Animal [] A=new Animal[3];
+           for(int i=0;i<A.Length;i++)
+           {
+               A[0]=new Dog("Dog");
+               A[1]=new Cat("Cat");
+               A[2]=new Frog("Frog");
+               Console.WriteLine("The "+A[i].Name+" makes "+A[i].MakeSound());
+           }
+        }
+    }
+}
