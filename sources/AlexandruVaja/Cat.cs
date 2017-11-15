72cdb47156e37304fe207567b22fd566b4596f50
alexvaja <33524281+alexvaja@users.noreply.github.com>
2017-11-15 18:53:07 +0200

    Am pus si tema mea cu animale salbatece

---
+++ sources/AlexandruVaja/Cat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tema1
+{
+    class Cat : Animal
+    {
+        public Cat(String name) => this.setName(name);
+        public override String MakeSound() => "meaumeaumeau";
+    }
+}
---
+++ sources/AlexandruVaja/Frog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tema1
+{
+    class Frog : Animal
+    {
+        public Frog(String name) => this.setName(name);
+        public override String MakeSound() => "guitguitguit";
+    }
+}
