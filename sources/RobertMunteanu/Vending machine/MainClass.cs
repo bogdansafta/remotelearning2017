0518f65df589b2665f78de756d2ff930fc086733
Robert Munteanu <munteanurobert96@yahoo.com>
2018-01-16 16:18:46 +0200

    Refactoring

---
+++ sources/RobertMunteanu/Vending machine/MainClass.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace VendingMachine{
+    class MainClass{
+        static void Main(string[] args)
+        {
+        }
+    }
+}
--- sources/RobertMunteanu/Vending machine/Position.cs
+++ sources/RobertMunteanu/Vending machine/Position.cs
@@ -3,22 +3,25 @@ namespace VendingMachine
 {
     public class Position : IEquatable<Position>
     {
-        public int Row { get; set; }
-        public int Column { get; set; }
-        public int Size { get; set; }
+        public int Row { get; private set; }
+        public int Column { get; private set; }
+        public int Size { get; private set; }
+        public int ID { get; private set; }
 
-        public Position(int Row = 0, int Column = 0, int Size)
+        public Position(int Row, int Column, int Size, int ID)
         {
             this.Row = Row;
             this.Column = Column;
             this.Size = Size;
+            this.ID = ID;
         }
 
-        public bool Equals(Position secondatyPosition)
+        public bool Equals(Position otherPosition)
         {
-            if (this.Row.Equals(secondatyPosition.Row) && this.Column.Equals(secondatyPosition.Column) && this.Size.Equals(secondatyPosition.Size))
-                return true;
-            else
-                return false;
+            return (this.Row.Equals(otherPosition.Row) && 
+            this.Column.Equals(otherPosition.Column) && 
+            this.Size.Equals(otherPosition.Size) &&
+            this.ID.Equals(otherPosition.ID)
+            );
         }
     }
