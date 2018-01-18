b2e4b99a78e939446f232049810da376e3ae4f1d
Bogdan Safta <bogdan.safta@iquestgroup.com>
2018-01-18 14:56:08 +0200

    Format payment terminl

--- sources/EmiliaPopa/VendingMachine/PaymentTerminal.cs
+++ sources/EmiliaPopa/VendingMachine/PaymentTerminal.cs
@@ -4,7 +4,7 @@ namespace VendingMachine
     public class PaymentTerminal
     {
         public string ErrorMessage = "The amount of money is lower than the product price";
-        public Dispenser Dispenser{ get; set; }
+        public Dispenser Dispenser { get; set; }
 
         public PaymentTerminal()
         {
@@ -12,30 +12,33 @@ namespace VendingMachine
         }
 
         public void Pay(int id, Payment payment)
-        { string line = Console.ReadLine();
-        double newAmount;
-        double amount=payment.MoneyPaid;
-         Product product= Dispenser.ContainableItemCollection.getItemById(id).Product;
-         double rest;
-         do{
-             rest=payment.change(amount, product.Price);
-             if(rest<0)
-             {
-                 Console.WriteLine(ErrorMessage);
-                 Console.WriteLine($"Enter more {rest}");
-                 newAmount=int.Parse(line);
+        {
+            string line = Console.ReadLine();
+            double newAmount;
+            double amount = payment.MoneyPaid;
+            Product product = Dispenser.ContainableItemCollection.getItemById(id).Product;
+            double rest;
+            do
+            {
+                rest = payment.change(amount, product.Price);
+                if (rest < 0)
+                {
+                    Console.WriteLine(ErrorMessage);
+                    Console.WriteLine($"Enter more {rest}");
+                    newAmount = int.Parse(line);
 
-             }
-         }while(rest>=0);
+                }
+            } while (rest >= 0);
 
-         if( rest > 0){
-             Console.WriteLine("banii trebuie sa se intoarca la cumparator");
-         }
-         
-         Dispenser.dispense(id);
+            if (rest > 0)
+            {
+                Console.WriteLine("banii trebuie sa se intoarca la cumparator");
+            }
 
-         }
-        
+            Dispenser.dispense(id);
+
+        }
+
 
 
     }
